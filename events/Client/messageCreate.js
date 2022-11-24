3d12e4db34cba73992e3a99263cb83c6de10ee3e
Cloynn <grizzlizyping@gmail.com>
2022-11-24 19:51:05 +0700

    new

---
+++ events/Client/messageCreate.js
@@ -0,0 +1,21 @@
+const client = require('../../index');
+const { EmbedBuilder } = require('discord.js');
+
+client.on('messageCreate', async (message) => {
+  if(message.author.bot || !message.guild) return;
+
+  const [ cmd, ...args ] = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+
+  if(!command) return;
+  await command.run(client, message, args).catch((error) => {
+    message.channel.send({
+      embeds: [
+        new EmbedBuilder()
+        .setColor('Red')
+        .setDescription('Something went wrong with the commands.')
+      ]
+    });
+    console.log(error);
+  });
+});
---
+++ events/Client/ready.js
@@ -0,0 +1,5 @@
+const client = require('../../index');
+
+client.on('ready', () => {
+  console.log(`Ready and up as ${client.user.tag}!`);
+});
