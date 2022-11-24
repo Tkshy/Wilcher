b3f062d1e5606b9e88aa4c2d696618a8e0320f47
Cloynn <grizzlizyping@gmail.com>
2022-11-24 19:33:40 +0700

    added

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const { Client } = require('discord.js');
-const client = new Client({ intents: ['Guilds'] });
+const { Client, Collection } = require('discord.js');
+const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent'] });
+module.exports = client;
+
+client.commands = new Collection();
+client.config = require('./config');
+
+require('./handler')(client);
+
+client.login(client.config.token);
