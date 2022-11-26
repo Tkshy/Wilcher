7b6526ca739c959d7adb52be0858d34bc715c610
Cloynn <grizzlizyping@gmail.com>
2022-11-26 18:08:34 +0700

    Wilcher

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Client, Collection } = require('discord.js');
 const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent'] });
 module.exports = client;
+require('dotenv').config();
 
 client.commands = new Collection();
 client.config = require('./config');
 
 require('./handler')(client);
 
-client.login(client.config.token); 
+client.login(process.env.token); 
