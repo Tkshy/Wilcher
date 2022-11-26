f22893c59606c24cc88cef41637e72b94877c161
Cloynn <grizzlizyping@gmail.com>
2022-11-26 16:38:34 +0700

    Mira

---
+++ commands/utilities/ping.js
@@ -0,0 +1,15 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'ping',
+  aliases: [],
+  run: async (client, message) => {
+    message.channel.send({
+      embeds: [
+        new EmbedBuilder()
+        .setColor('DarkAqua')
+        .setDescription('Pong!')
+      ]
+    });
+  },
+};
