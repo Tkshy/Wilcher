01735d6f1519f6d7ea564581b833f0722a21fb8a
Cloynn <grizzlizyping@gmail.com>
2022-11-27 15:34:31 +0700

    Wilcher

--- events/Client/messageCreate.js
+++ events/Client/messageCreate.js
@@ -2,7 +2,7 @@ const client = require('../../index');
 const { EmbedBuilder } = require('discord.js');
 
 client.on('messageCreate', async (message) => {
-  if(message.author.bot || !message.guild) return;
+  if(message.author.bot || !message.guild || !message.content.toLowerCase().startsWith(client.config.prefix)) return;
 
   const [ cmd, ...args ] = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
