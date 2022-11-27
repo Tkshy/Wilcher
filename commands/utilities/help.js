6e2f7d01cd097fac1b3589867f520382a0a05807
Cloynn <grizzlizyping@gmail.com>
2022-11-27 21:40:44 +0700

    Wilcher

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -6,34 +6,51 @@ module.exports = {
   owner: false,
   description: 'See information about the command',
   run: async (client, message, args) => {
-    let cmd = args[0];
+    let choice = args[0];
+    if(choice) {
+      let cmd = args[0];
 
-    if(!cmd) {
-      return message.channel.send({
-        embeds: [
-          new EmbedBuilder()
-          .setColor('Red')
-          .setDescription('Please mention command that you want to see the description!')
-        ]
-      });
-    };
+      if(!cmd) {
+        return message.channel.send({
+          embeds: [
+            new EmbedBuilder()
+            .setColor('Red')
+            .setDescription('Please mention command that you want to see the description!')
+          ]
+        });
+      };
 
-    let command = client.commands.get([...client.commands.keys()].find(x => x.toLowerCase() === cmd.toLowerCase()));
-    if(!command) {
-      return message.channel.send({
-        embeds: [
-          new EmbedBuilder()
-          .setColor('Red')
-          .setDescription('Command not found.')
-        ]
-      });
+      let command = client.commands.get([...client.commands.keys()].find(x => x.toLowerCase() === cmd.toLowerCase()));
+      if(!command) {
+        return message.channel.send({
+          embeds: [
+            new EmbedBuilder()
+            .setColor('Red')
+            .setDescription('Command not found.')
+          ]
+        });
+      } else {
+        message.channel.send({
+          embeds: [
+            new EmbedBuilder()
+            .setTitle(`Information about the command`)
+            .setColor('DarkAqua')
+            .setDescription(`Command name: ${command.name}\nCommand description: ${command.description}\nCommand owner: ${command.owner ? 'Yes' : 'No'}`)
+            .setFooter({ text: `Requested by ${message.author.tag}` })
+            .setTimestamp()
+          ]
+        });
+      };
     } else {
-      message.channel.send({
+      return message.channel.send({
         embeds: [
           new EmbedBuilder()
-          .setTitle(`${command.name} Infomation`)
           .setColor('DarkAqua')
-          .setDescription(`Description: ${command.description}`)
+          .setAuthor({ name: 'List of commands from Wilcher', iconURL: client.user.displayAvatarURL({ format: 'png' })})
+          .setThumbnail(client.user.displayAvatarURL({ format: 'png' }))
+          .setDescription('This is a description')
+          .setFooter({ text: `Use ${client.config.prefix}help <cmd name> for help`, iconURL: client.user.displayAvatarURL({ format: 'png' })})
+          .setTimestamp()
         ]
       });
     };
