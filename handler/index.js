73a629d9bbea7ad480f43201da07b03e43980dcf
Tokioshy <grizzlizyping@gmail.com>
2022-12-03 14:29:27 +0700

    Wilcher

--- handler/index.js
+++ handler/index.js
@@ -17,4 +17,52 @@ module.exports = async (client) => {
 
   const eventFiles = await globPromise(`${process.cwd()}/events/**/*.js`);
   eventFiles.map((value) => require(value));
+
+  const fun = await globPromise(`${process.cwd()}/commands/fun/*.js`); // fun
+  fun.map((value) => {
+    const file = require(value);
+    const splitted = value.split('/');
+    const directory = splitted[splitted.length - 2];
+
+    if(file.name) {
+      const properties = { directory,  ...file };
+      client.fun.set(file.name, properties);
+    };
+  });
+
+  const moderation = await globPromise(`${process.cwd()}/commands/moderation/*.js`); // moderation
+  moderation.map((value) => {
+    const file = require(value);
+    const splitted = value.split('/');
+    const directory = splitted[splitted.length - 2];
+
+    if(file.name) {
+      const properties = { directory,  ...file };
+      client.moderation.set(file.name, properties);
+    };
+  });
+
+  const utility = await globPromise(`${process.cwd()}/commands/utility/*.js`); // utility
+  utility.map((value) => {
+    const file = require(value);
+    const splitted = value.split('/');
+    const directory = splitted[splitted.length - 2];
+
+    if(file.name) {
+      const properties = { directory,  ...file };
+      client.utility.set(file.name, properties);
+    };
+  });
+
+  const image = await globPromise(`${process.cwd()}/commands/utility/*.js`); // image
+  image.map((value) => {
+    const file = require(value);
+    const splitted = value.split('/');
+    const directory = splitted[splitted.length - 2];
+
+    if(file.name) {
+      const properties = { directory,  ...file };
+      client.image.set(file.name, properties);
+    };
+  });
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { Client, Collection } = require('discord.js');
-const client = new Client({ intents: 3276796 });
+const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent', 'GuildBans'] });
 module.exports = client;
 require('dotenv').config();
 
 client.commands = new Collection();
+client.fun = new Collection();
+client.moderation = new Collection();
+client.image = new Collection();
+client.utility = new Collection();
 client.config = require('./config');
 
 require('./handler')(client);
