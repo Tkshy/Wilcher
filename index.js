aae14c3a1f89f4acc8c919fced92155d04457b45
Tokioshy <grizzlizyping@gmail.com>
2022-12-03 14:07:14 +0700

    Wilcher

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Collection } = require('discord.js');
-const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent', 'GuildBans'] });
+const client = new Client({ intents: 3276796 });
 module.exports = client;
 require('dotenv').config();
 
